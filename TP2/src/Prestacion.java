16ba301ac19f79ba5ee660108f7fdb6ba4c136ad
ivanmengual <ivanmengual97@gmail.com>
2016-05-28 15:56:22 -0300

    lalal

--- TP2/src/Prestacion.java
+++ TP2/src/Prestacion.java
@@ -7,8 +7,24 @@ public abstract class Prestacion {
 	private Integer id;
 		
 	public String toString(){
-		return nombre;
+		return getNombre();
 		
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
 	}
 		
 	//aca me falta el visitor pero no se como se escribe. Invento el nombre
