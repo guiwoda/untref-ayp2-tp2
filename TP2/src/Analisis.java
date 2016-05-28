1e130714570acb031985e064b200cdc92ff1b043
te <te@mail>
2016-05-28 16:07:01 -0400

    comit

--- TP2/src/Analisis.java
+++ TP2/src/Analisis.java
@@ -2,8 +2,8 @@ import java.sql.Date;
 
 public class Analisis extends Prestacion {
 
-	private Integer id;
-	private String nombre;
+	//private Integer id;
+	//private String nombre;
 	private Integer valorNormalMinimo;
 	private Integer valorNormalMaximo;
 	private String indicaciones;
@@ -13,8 +13,14 @@ public class Analisis extends Prestacion {
 	private Resultado resultado;
 	// A ver
 	private String normalidad;
-	
-	
+
+	public Analisis(Integer id, String nombre, Integer valorNormalMinimo, Integer valorNormalMaximo, String indicaciones){
+		super.setId(id);
+		super.setNombre(nombre);
+		this.valorNormalMinimo=valorNormalMinimo;
+		this.valorNormalMaximo=valorNormalMaximo;
+		this.indicaciones=indicaciones;
+	} 
 	
 	public void evaluarNormalidad() {
 		if ((resultado.getValorMedido() < this.valorNormalMaximo)
@@ -28,7 +34,7 @@ public class Analisis extends Prestacion {
 	// Para mostrar resultados
 	public String toString() {
 		// Falta inicializar normalidad
-		return nombre + " " + resultado.getValorMedido().toString() + " " + this.normalidad
+		return  " " + resultado.getValorMedido().toString() + " " + this.normalidad
 				+ " dentro del valor minimo " + this.valorNormalMinimo.toString() + " y valor maximo  "
 				+ this.valorNormalMaximo.toString();
 	}
