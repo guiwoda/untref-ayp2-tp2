c01c49d2715da9675d78550874f0828699ce005e
te <te@mail>
2016-05-31 00:15:48 -0400

    Agrego Test inicial de Analisis

--- TP2/tests/AnalisisTest.java
+++ TP2/tests/AnalisisTest.java
@@ -2,13 +2,35 @@
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class AnalisisTest {
+	private Analisis glucemia;
+	private Resultado resultado;
+
+	@Before
+	public void creoAnalisisYResultado() {
+		glucemia = new Analisis(4, "glucemia", 10, 100, "Con globulos en estado critico.");
+		resultado = new ResultadoAnalisis();
+		//no le puedo setear un ValorMedido a resultado; no puedo probar Analisis.mostrar()
+		//no faltaria un constructor de ResultadoAnalisis que nos permita agregarle el valorMedido
+	}
 
 	@Test
-	public void test() {
-		fail("No implementado aun");
+	public void obtenerValorNormalMinimo(){
+		assertEquals(10, glucemia.getValorNormalMinimo(), 1.0);
 	}
+	
+	@Test
+	public void obtenerValorNormalMaximo(){
+		assertEquals(100, glucemia.getValorNormalMaximo(), 1.0);
+	}
+	
+//	@Test
+//	public void mostrar(){
+//		
+//	}
+	
 
 }
