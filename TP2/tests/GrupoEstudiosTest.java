58ab030a56301612bf3ba07dcf42b9631380f659
Guido Contreras Woda <guiwoda@gmail.com>
2016-06-05 18:55:20 -0300

    Test mostrar resultados en grupo de estudios

--- TP2/tests/GrupoEstudiosTest.java
+++ TP2/tests/GrupoEstudiosTest.java
@@ -1,8 +1,10 @@
 
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -13,22 +15,30 @@ public class GrupoEstudiosTest {
 	private List<TipoPrestacion> prestaciones;
 	private Estudio cardio;
 	private Estudio radio;
-	private ResultadoEstudio resultado;
 	@Before
 	public void setUp(){
 		cardio = new EstudioCardiologico("electro", "Debe tener una linea que sube y baja");
 		radio = new EstudioRadiologico("Placa dental", "Debe tener de 2 a 128 dientes");
-		resultado = new ResultadoEstudio(true);
-		cardio.cargarResultado(resultado);
-		radio.cargarResultado(resultado);
+
 		prestaciones = new LinkedList<>();
 		prestaciones.add(cardio);
 		prestaciones.add(radio);
+
 		grupo = new GrupoEstudios("Electro y placa dental", prestaciones);
 	}
 	@Test
-	public void test() {
-		assertEquals("", grupo.mostrar(resultado));
+	public void testMostrar() {
+		Map<TipoPrestacion, Resultado> mapa = new HashMap<>();
+		ResultadoEstudio subresultado = new ResultadoEstudio(true);
+
+		mapa.put(cardio, subresultado);
+		mapa.put(radio, subresultado);
+
+		ResultadoGrupoEstudios resultado = new ResultadoGrupoEstudios(mapa);
+
+		assertEquals("Electro y placa dental\n" +
+			"  Estudio Cardiológico: electro Normal Debe tener una linea que sube y baja\n" +
+			"  Estudio Radiologico: Placa dental Normal Debe tener de 2 a 128 dientes\n", grupo.mostrar(resultado));
 	}
 
 }
